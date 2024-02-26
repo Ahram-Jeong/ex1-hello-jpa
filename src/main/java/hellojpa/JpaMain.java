75f7f73e51ec3da620ca623038c509f63d978cda
Ahram-Jeong <ahram1215@gmail.com>
2024-02-26 11:16:19 +0900

    20240226 - JPA 기본_insert

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,32 @@
 package hellojpa;
 
-public class JpaMain {
-    
-    public static void main(String[] args) {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ash"); // 애플리케이션 로딩 시점에 딱 하나만 생성
+        EntityManager em = emf.createEntityManager(); // 트랜잭션 단위 별로 생성
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("WOODZ");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
     }
 }
